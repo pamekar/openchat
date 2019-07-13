7451c611a66b495f9eac6de5db5a18ed250dac22
GreenWhite <greenwhitedev@gmail.com>
2019-07-14 00:03:05 +0100

    Chat View

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="username" content="{{\Illuminate\Support\Facades\Cookie::get('username')}}" id="username">
+    <meta name="user_id" content="{{\Illuminate\Support\Facades\Cookie::get('user_id')}}" id="user_id">
+
+    <title>{{config('app.name')}}</title>
+    <link href="{{asset('css/font-awesome.css')}}" type="text/css" rel="stylesheet">
+    <link href="{{mix('css/app.css')}}" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+<div id="app">
+    <chat></chat>
+</div>
+</body>
+<script src="{{mix('js/app.js')}}"></script>
+</html>
