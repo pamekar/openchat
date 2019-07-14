e65d5b003e4173f8c2c9a72c7114d8396c2b2c9b
GreenWhite <greenwhitedev@gmail.com>
2019-07-14 07:05:55 +0100

    File Rename/Changed page title

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,10 +2,13 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <meta name="username" content="{{\Illuminate\Support\Facades\Cookie::get('username')}}" id="username">
     <meta name="user_id" content="{{\Illuminate\Support\Facades\Cookie::get('user_id')}}" id="user_id">
 
-    <title>{{config('app.name')}}</title>
+    <title>{{\Illuminate\Support\Facades\Cookie::get('username')." - ".config('app.name')}}</title>
     <link href="{{asset('css/font-awesome.css')}}" type="text/css" rel="stylesheet">
     <link href="{{mix('css/app.css')}}" rel="stylesheet" id="bootstrap-css">
 </head>
